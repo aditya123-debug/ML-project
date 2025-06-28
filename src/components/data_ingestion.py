e57b0565dea1f594d23fe3ecbebdb39df27d31da
Aditya Reddy <ADI@Harshs-MacBook-Air.local>
2025-06-28 15:45:46 +0530

    model training done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation,DataTransfromationConfig
+from src.components.model_trainer import ModelTrainerConfig,Model_Trainer
 
 @dataclass
 class DataIngestionConfig:
@@ -49,6 +50,9 @@ if __name__=='__main__':
     train_data,test_data=obj.initiate_data_ingestion() 
     
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer=Model_Trainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
                      
 
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 import dill
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 def save_obj(file_path,obj):
@@ -17,3 +19,20 @@ def save_obj(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models: dict):
+    '''
+    This function evaluates multiple regression models and returns a dictionary
+    of model names and their corresponding R² scores.
+    '''
+    try:
+        report = {}
+        for name, model in models.items():
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            score = r2_score(y_test, y_pred)
+            report[name] = score
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
+        
